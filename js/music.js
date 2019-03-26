60df0c86cc3249c4f789829cc4a1cd5f9fe38c18
Aldair <aldairgarcia803@gmail.com>
2019-03-26 03:09:24 +0000

    dsd

---
+++ js/music.js
@@ -0,0 +1,27 @@
+/*global $*/
+
+var x = document.getElementById("myAudio");
+
+function enableControls() { 
+  x.pause();
+} 
+
+function disableControls() { 
+  x.play();
+} 
+ 
+var clicknumber=0;
+$(".playstop").click(function(){
+    
+    
+    var num=clicknumber+++1;
+    $("h1").html(num);
+    if(num%2 !== 0){
+        
+        enableControls();
+        }
+    else if (num%2 === 0){
+        
+        disableControls();
+        }
+    });
